47f73bdba750ca63c96a71833f4e55f45ec73d4f
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2024-12-22 01:04:33 +0600

    elkano64 client initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl my-8 text-center font-semibold'>Elkano 64 client initialized</h1>
+      <div className='text-center'>
+        <button className='btn btn-primary'>Reservation</button>
+      </div>
+    </>
+  )
+}
+
+export default App
