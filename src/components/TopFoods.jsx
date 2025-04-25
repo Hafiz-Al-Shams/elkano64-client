8aa4b7629483c64723f93828cf4526776236995b
Hafiz Al Shams <hafizalshams1917@gmail.com>
2025-04-25 19:44:52 +0600

    modified topfoods content color

--- src/components/TopFoods.jsx
+++ src/components/TopFoods.jsx
@@ -20,12 +20,12 @@ const TopFoods = () => {
 
     return (
         <div className="w-11/12 mx-auto">
-            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center pb-4 md:pb-6 lg:pb-10 pt-10 md:pt-14 lg:pt-24">Our 6 top-selling Food Items</h2>
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center pb-4 md:pb-6 lg:pb-10 pt-10 md:pt-14 lg:pt-24 underline">Our 6 top-selling Food Items</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 md:gap-y-12 lg:gap-y-24 pb-6 md:pb-7 lg:pb-10 pl-4 md:pl-7 lg:pl-12">
                 {
                     topFoods.map(food => (
-                        <div key={food._id} className="card card-compact bg-white p-0.5 md:p-1.5 lg:p-3.5 w-64 md:w-72 lg:w-96 shadow-xl">
+                        <div key={food._id} className="card card-compact bg-base-100 border-2 p-0.5 md:p-1.5 lg:p-3.5 w-64 md:w-72 lg:w-96 shadow-xl">
                             <figure className="">
                                 <img
                                     className="p-4 rounded-3xl w-60 md:w-64 lg:w-full"
@@ -34,10 +34,10 @@ const TopFoods = () => {
                             </figure>
                             <div className="card-body">
                                 <h2 className="card-title md:text-xl lg:text-2xl md:font-semibold lg:font-bold">{food.name}</h2>
-                                <p className="font-medium text-xs lg:text-lg text-gray-700">Price: {food.price}</p>
-                                <p className="font-medium text-xs lg:text-lg text-gray-700">Purchase Count: {food.purchaseCount}</p>
-                                <p className="font-medium text-xs lg:text-lg text-gray-700">Available Quantity: {food.availableQuantity}</p>
-                                <p className="font-medium text-xs lg:text-lg text-gray-700">Description: {food.description}</p>
+                                <p className="font-medium text-xs lg:text-lg text-base-content">Price: {food.price}</p>
+                                <p className="font-medium text-xs lg:text-lg text-base-content">Purchase Count: {food.purchaseCount}</p>
+                                <p className="font-medium text-xs lg:text-lg text-base-content">Available Quantity: {food.availableQuantity}</p>
+                                <p className="font-medium text-xs lg:text-lg text-base-content">Description: {food.description}</p>
                                 <div className="card-actions justify-end mt-2">
                                     <Link to={`/foods/${food._id}`}>
                                         <button className="btn btn-primary btn-xs md:btn-sm lg:btn-md lg:px-4 lg:text-lg">Details</button>
